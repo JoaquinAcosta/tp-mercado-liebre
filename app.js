bcb7811e37ea5edd5b4451dbf4e62c71164f70f7
JoaquinAcosta <joaquinnof14@gmail.com>
2022-06-28 08:32:37 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express()
+const port = 3030
+
+app.use(express.static('public'))
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:3030${port}`));
