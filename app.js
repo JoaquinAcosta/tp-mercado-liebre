4ec77c9e4dde39134e1290f74a5f38a196854f48
JoaquinAcosta <joaquinnof14@gmail.com>
2022-07-11 10:38:35 -0300

    agregue styles, general y home

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.use(express.static('public'))
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 
-app.listen(port, () => console.log(`Servidor corriendo en http://localhost:3030${port}`));
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`));
